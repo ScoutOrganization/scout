f9939716bf108fea1c5ce7f7594d0fbb7e544e05
sakurakiyama <sakura.bowden@gmail.com>
2023-01-30 15:23:09 -0500

    completed boilerplate

---
+++ server/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+const apiRouter = require('./routes/api'); 
+const cors = require('cors'); 
+
+// parses JSON from incoming request
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// handle requests for static files
+app.use(express.static(path.resolve(__dirname, '../client')));
+
+// define route handlers
+app.use('/api', apiRouter, (req, res) => {
+    return res.status(200);
+    });
+
+// catch-all error handler
+app.get('*', (req, res) => {
+    return res.status(404).send('This is not the page you\'re looking for...')})
+  
+  // Global error handling middleware
+  app.use((err, req, res, next) => {
+      const defaultErr = {
+        log: 'Express error handler caught unknown middleware error',
+        status: 400,
+        message: { err: 'An error occurred' },
+      };
+      const errorObj = Object.assign({}, defaultErr, err);
+      console.log(errorObj.log);
+      return res.status(errorObj.status).json(errorObj.message);
+    });
+  
+  app.listen(PORT, () => {
+      console.log('Server listening on port 3000');
+    });
+    
+    module.exports = app;
---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: path.resolve(__dirname, 'client/src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  target: 'web',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: 'client/public/index.html',
+    }),
+  ],
+  devServer: {
+    host: 'localhost',
+    port: '8080',
+    historyApiFallback: true,
+    static: {
+      directory: path.resolve(__dirname, 'build'),
+      publicPath: 'build',
+    },
+    open: true,
+    hot: true,
+    liveReload: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /.(css|scss)$/,
+        exclude: [/node_modules/, /client\/stylesheets\/modules/],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /.(css|scss)$/,
+        include: [/client\/stylesheets\/modules/],
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+          {
+            loader: 'react-svg-loader',
+            options: {
+              jsx: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
