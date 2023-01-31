b09d5a2e4d6ea4c1fc7bc7d21c2effe1857b7c25
sakurakiyama <sakura.bowden@gmail.com>
2023-01-31 17:54:53 -0500

    bulletin board logic for generating posts from database

--- client/src/Bulletin.jsx
+++ client/src/Bulletin.jsx
@@ -1,19 +1,32 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import '../public/styles/bulletin.scss';
 import NavBar from './NavBar';
 import { useAuth0 } from '@auth0/auth0-react';
 import Post from './Post';
+import axios from 'axios';
 
 
 export default function Bulletin() {
-
   const { user, isAuthenticated, loginWithRedirect, isLoading } = useAuth0();
-  const [location, setLocation] = useState(''); 
+  const [location, setLocation] = useState('');
+  const [posts, setPosts] = useState([]); 
 
   function handleFormSubmit(event) {
     event.preventDefault();
-    setLocation(event.target[0].value)
+    setLocation(event.target[0].value);
   }
+
+  useEffect(() => {
+    async function getPosts() {
+      try {
+        const response = await axios.get('http://localhost:3000/bulletin');
+        setPosts([...response.data]); 
+      } catch (err) {
+        console.log('no posts recieved');
+      }
+    }
+    getPosts(); 
+  }, []); 
 
   return (
     <div className='bulletinContainer'>
@@ -21,22 +34,18 @@ export default function Bulletin() {
       <h1>Bulletin Board</h1>
       <div className='bulletinWrapper'>
         <div className='locationWrapper'>
-        <form onSubmit={handleFormSubmit}>
-          <label id='locationLabel'>
-            LOCATION 
-          </label>
-          < br />
-          <input
-          id='locationInput'
-          type='text'
-          >
-          </input>
-          < br />
-          <button id='submitLocation' type='submit' role='button'>Filter</button>
-        </form>
+          <form onSubmit={handleFormSubmit}>
+            <label id='locationLabel'>LOCATION</label>
+            <br />
+            <input id='locationInput' type='text'></input>
+            <br />
+            <button id='submitLocation' type='submit' role='button'>
+              Filter
+            </button>
+          </form>
         </div>
         <div className='allPostsContainer'>
-            <Post />
+          <Post posts={posts} />
         </div>
       </div>
     </div>
--- client/src/Post.jsx
+++ client/src/Post.jsx
@@ -1,40 +1,29 @@
 import React from 'react';
 import '../public/styles/post.scss';
-import axios from 'axios';
 
-export default function Post() {
-
-  let response = ''; 
-  async function getPosts() {
-    try {
-      response = await axios.get('http://localhost:3000/bulletin');
-    } catch (err) {
-      console.log('no posts recieved');
-    }
-  }
-
+export default function Post({posts}) {
+  console.log('all posts', posts)
   return (
     <>
-    {response.map((post) => {
-      ({
+    {posts.map((post) => {
+      console.log('each post', post)
+      const {
         email,
         first_name,
-        last_name,
-        item_lost,
         item_description,
+        item_lost,
+        last_name,
         location,
         _id,
-      } = post);
+      } = post
       return (
-        <div className='postContainer'>
         <div className='postWrapper' key={_id}>
-          <p>Hi I'm a post</p>
-          Full Name: {(first_Name, last_Name)} <br />
+          Full Name: {(first_name, last_name)} <br />
           Item Lost: {item_lost} <br />
           Location: {location} <br />
           Item Description: {item_description} <br />
+          Contact: {email}
         </div>
-      </div>
       )
     })}
     </>
