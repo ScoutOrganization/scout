ced8114e79fa436ebf04d715d838157b77bfabbf
Josh <you@example.com>
2023-01-31 11:00:02 -0500

    Oauth intergrated

--- client/src/Landing.jsx
+++ client/src/Landing.jsx
@@ -1,22 +1,35 @@
 import React from 'react';
 import '../public/styles/landing.scss';
 import sunglasses from '../public/images/sunglasses.jpg';
+import { useAuth0 } from '@auth0/auth0-react';
 
 export default function Landing() {
+  const { loginWithRedirect } = useAuth0();
   return (
     <div className='landingContainer'>
       <div className='right'>
-      <img id='sunglasses' alt='sunglasses' src={sunglasses}></img>
+        <img id='sunglasses' alt='sunglasses' src={sunglasses}></img>
       </div>
 
       <div className='left'>
         <h3>WELCOME TO</h3>
         <h1>Scout</h1>
-        <h3>Welcome to Scout, the community-driven platform that helps reunite people with their lost belongings. Our mission is to make it easier for individuals to recover lost items through the power of community. Whether you've lost your keys, wallet, phone, or any other important item, our platform allows users to report lost items by creating a post, which can be viewed by members of our community. With a network of people working together, we increase the chances of finding lost items and bringing them back to their rightful owners. Join us in our mission to make the world a little less lost.</h3>
-        <br />  <br />
-        <button class='startBtn'>GET STARTED</button>
+        <h3>
+          Welcome to Scout, the community-driven platform that helps reunite
+          people with their lost belongings. Our mission is to make it easier
+          for individuals to recover lost items through the power of community.
+          Whether you've lost your keys, wallet, phone, or any other important
+          item, our platform allows users to report lost items by creating a
+          post, which can be viewed by members of our community. With a network
+          of people working together, we increase the chances of finding lost
+          items and bringing them back to their rightful owners. Join us in our
+          mission to make the world a little less lost.
+        </h3>
+        <br /> <br />
+        <button className='startBtn' onClick={() => loginWithRedirect()}>
+          GET STARTED
+        </button>
       </div>
     </div>
   );
 }
-
--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = 3000;
-const apiRouter = require('./routes/api'); 
-const cors = require('cors'); 
+// const apiRouter = require('./routes/api');
+const cors = require('cors');
 
 // parses JSON from incoming request
 app.use(express.json());
@@ -14,28 +14,29 @@ app.use(cors());
 app.use(express.static(path.resolve(__dirname, '../client')));
 
 // define route handlers
-app.use('/api', apiRouter, (req, res) => {
-    return res.status(200);
-    });
+// app.use('/api', apiRouter, (req, res) => {
+//     return res.status(200);
+//     });
 
 // catch-all error handler
 app.get('*', (req, res) => {
-    return res.status(404).send('This is not the page you\'re looking for...')})
-  
-  // Global error handling middleware
-  app.use((err, req, res, next) => {
-      const defaultErr = {
-        log: 'Express error handler caught unknown middleware error',
-        status: 400,
-        message: { err: 'An error occurred' },
-      };
-      const errorObj = Object.assign({}, defaultErr, err);
-      console.log(errorObj.log);
-      return res.status(errorObj.status).json(errorObj.message);
-    });
-  
-  app.listen(PORT, () => {
-      console.log('Server listening on port 3000');
-    });
-    
-    module.exports = app;
+  return res.status(404).send("This is not the page you're looking for...");
+});
+
+// Global error handling middleware
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(PORT, () => {
+  console.log('Server listening on port 3000');
+});
+
+module.exports = app;
