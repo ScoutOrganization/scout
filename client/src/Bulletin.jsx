4ea4c55713808e1d3d50eee3833f043431409b2f
sakurakiyama <sakura.bowden@gmail.com>
2023-01-31 22:53:18 -0500

    adding styling and logic to user profile and bulletin board

--- client/src/Bulletin.jsx
+++ client/src/Bulletin.jsx
@@ -4,12 +4,14 @@ import NavBar from './NavBar';
 import { useAuth0 } from '@auth0/auth0-react';
 import Post from './Post';
 import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 
 export default function Bulletin() {
   const { user, isAuthenticated, loginWithRedirect, isLoading } = useAuth0();
   const [location, setLocation] = useState('');
   const [posts, setPosts] = useState([]); 
+  const history = useHistory(); 
 
   function handleFormSubmit(event) {
     event.preventDefault();
@@ -39,10 +41,13 @@ export default function Bulletin() {
             <br />
             <input id='locationInput' type='text'></input>
             <br />
-            <button id='submitLocation' type='submit' role='button'>
+            <button className='bulletinBtn' type='submit' role='button'>
               Filter
             </button>
           </form>
+          <div id='createPostContainer'> 
+            <button className='bulletinBtn' type='button' onClick={() => {history.push('/createPost')}}>CREATE POST </button>
+          </div>
         </div>
         <div className='allPostsContainer'>
           <Post posts={posts} />
