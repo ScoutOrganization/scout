991e417e5192265d6a8c1d28ea0e192dc271e381
Arthur Huynh <arthurhuynh@Arthurs-Computer.local>
2023-02-01 15:50:05 -0500

    adds update button to user profile posts

--- client/src/UserPosts.jsx
+++ client/src/UserPosts.jsx
@@ -2,38 +2,22 @@ import React, { useState, useEffect } from 'react';
 import '../public/styles/userPosts.scss';
 import { RiDeleteBin5Fill } from 'react-icons/ri';
 import axios from 'axios';
+import EdiText from 'react-editext';
+
 // import { useAuth0 } from '@auth0/auth0-react';
 
 export default function UserPosts({ userPosts }) {
-  // changing the text in the button
-  const [toggleButton, setToggleButton] = useState('EDIT');
-  // noting when in edit mode
-  const [editMode, setEditMode] = useState(false);
-  const [description, setDescription] = useState();
-
   async function handleDeletePost(postID) {
     await axios.delete('http://localhost:3000/userPosts', {
       data: { postID: postID },
     });
   }
 
-  async function editButton(postId) {
-    if (toggleButton === 'EDIT') {
-      setToggleButton('SAVE');
-      // put the item description in a form as a placeholder.
-      userPosts.forEach((element) => {
-        if (Object.values(element).includes(postId)) {
-          console.log('description in state: ', element.item_description);
-          return setDescription(element.item_description);
-        }
-      });
-    } else {
-      setToggleButton('EDIT');
-      // send off an axios post to backend with new data.
-    }
+  async function updateDescription(val) {
+    await axios.post('http://localhost:3000/userPosts', {
+      data: { item_description: val },
+    });
   }
-
-  useEffect(() => {}, []);
 
   return (
     <>
@@ -53,16 +37,14 @@ export default function UserPosts({ userPosts }) {
             Contact: {email} <br />
             Location: {location} <br />
             Item Lost: {item_lost} <br />
-            Item Description: {item_description} <br />
+            Item Description:
+            <EdiText
+              type='text'
+              value={item_description}
+              onSave={updateDescription}
+            />
+            <br />
             <div className='buttonContainer'>
-              <button
-                id='foundBtn'
-                onClick={() => {
-                  editButton(_id);
-                }}
-              >
-                {toggleButton}
-              </button>
               <button
                 id='foundBtn'
                 onClick={() => {
