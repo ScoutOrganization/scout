60108b20da95a45e24477cb218e79ff2671cf816
sakurakiyama <sakura.bowden@gmail.com>
2023-01-31 20:30:07 -0500

    adding user profile functionality

--- client/src/NavBar.jsx
+++ client/src/NavBar.jsx
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../public/styles/navBar.scss';
 import raccoon from '../public/images/raccoon.png';
-import { useAuth0 } from "@auth0/auth0-react";
+import { useAuth0 } from '@auth0/auth0-react';
+import { useHistory } from 'react-router-dom';
+
 
 export default function NavBar() {
-  const { logout } = useAuth0();
+  const { user, isAuthenticated, logout, loginWithRedirect, isLoading } = useAuth0();
+  const [userPhoto, setUserPhoto] = useState();
+  const history = useHistory(); 
+
+  useEffect(() => {
+    if (user) {
+      setUserPhoto(user.picture);
+    }
+  }, [user]);
 
   return (
     <div className='navBarContainer'>
       <div className='left'>
-        <a href='/'><img src={raccoon} alt='logo' id='logo'></img></a>
+        <a href='/'>
+          <img src={raccoon} alt='logo' id='logo'></img>
+        </a>
       </div>
       <div className='spacer'></div>
-      <div className="right">
-      <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
-      LOG OUT
-    </button>
+      <div className='right'>
+        <button id ='logout'
+          onClick={() =>
+            logout({ logoutParams: { returnTo: window.location.origin } })
+          }
+        >
+          LOG OUT
+        </button>
+        <button id='profileBtn' onClick={() => history.push('/profile')}><img id='userPhoto' src={userPhoto} /> </button>
       </div>
     </div>
   );
