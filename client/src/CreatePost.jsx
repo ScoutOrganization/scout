ce3df0ae6eb2e94340a428acfb14046cfc63508d
jake martin''test <jakem417@icloud.com>
2023-01-31 18:23:19 -0700

    this add functionality to createpost

--- client/src/CreatePost.jsx
+++ client/src/CreatePost.jsx
@@ -2,128 +2,127 @@ import React, { useState } from 'react';
 import '../public/styles/CreatePost.scss';
 import post from '../public/images/post.jpg';
 import { useAuth0 } from '@auth0/auth0-react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
-const CreatePost = () => {
+const CreatePost =  () => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [itemName, setItemName] = useState('');
   const [location, setLocation] = useState('');
   const [details, setDetails] = useState('');
   const { user, isAuthenticated, loginWithRedirect, isLoading } = useAuth0();
+  const history = useHistory();
 
-  console.log(user); 
-  const submitForm = () => {
+
+  const submitForm = async () => {
     const requestBody = {
-      firstName,
-      lastName,
-      itemName,
+      email: user.email,
+      first_name: firstName,
+      last_name: lastName,
+      item_lost: itemName,
       location,
-      details,
+      item_description: details,
     };
-    console.log(requestBody);
-
-    // fetch('http://localhost:8080/api/login', {
-    // 	headers: {
-    // 		Accept: 'application/json',
-    // 		'Content-Type': 'application/json',
-    // 	},
-    // 	method: 'POST',
-    // 	mode: 'cors',
-    // 	body: JSON.stringify(requestBody),
-    // })
-    // 	.then((data) => data.json())
-    // 	.then((data) => {
-    // 		console.log(data);
-    // 	})
-    // 	.catch((err) => {
-    // 		console.error('err', err);
-    // 	});
+    
+    const sendPost = async () => {
+      try {
+        await axios.post('http://localhost:3000/createPost', requestBody); 
+      } catch (err) {
+        console.log('unsuccessful post');
+      }
+    }
+    sendPost();
 
     setFirstName('');
     setLastName('');
     setItemName('');
     setLocation('');
     setDetails('');
+    history.push('/bulletin')
   };
-
-  return (
-    <div className='postContainer'>
-      <div className='photo'>
-        <img id='board' src={post}></img>
+  if (user) {
+    return (
+      <div className='postContainer'>
+        <div className='photo'>
+          <img id='board' src={post}></img>
+        </div>
+        <div className='post'>
+          <h1>Create a Post</h1>
+          <br />
+          <label className='label' htmlFor='first'>
+            FIRST NAME
+          </label>
+          <br />
+          <textarea
+            id='first'
+            className='input'
+            onInput={(e) => {
+              setFirstName(e.currentTarget.value);
+            }}
+            value={firstName}
+            placeholder={user.given_name}
+          ></textarea>
+          <br />
+          <label className='label' htmlFor='last'>
+            LAST NAME
+          </label>
+          <br />
+          <textarea
+            id='last'
+            className='input'
+            onInput={(e) => {
+              setLastName(e.currentTarget.value);
+            }}
+            value={lastName}
+            placeholder={user.family_name}
+          ></textarea>
+          <br />
+          <label className='label' htmlFor='location'>
+            LOCATION
+          </label>
+          <br />
+          <textarea
+            id='location'
+            className='input'
+            onInput={(e) => {
+              setLocation(e.currentTarget.value);
+            }}
+            value={location}
+          ></textarea>
+          <br />
+          <label className='label' htmlFor='item'>
+            ITEM LOST
+          </label>
+          <br />
+          <textarea
+            id='item'
+            className='input'
+            onInput={(e) => {
+              setItemName(e.currentTarget.value);
+            }}
+            value={itemName}
+          ></textarea>
+          <br />
+          <label className='label' htmlFor='details'>
+            ADDITIONAL INFORMATION
+          </label>
+          <br />
+          <textarea
+            id='details'
+            onInput={(e) => {
+              setDetails(e.currentTarget.value);
+            }}
+            value={details}
+          ></textarea>
+          <br />
+          <button id='button' onClick={submitForm}>
+            POST
+          </button>
+        </div>
       </div>
-      <div className='post'>
-        <h1>Create a Post</h1>
-        <br />
-        <label className='label' htmlFor='first'>
-          FIRST NAME
-        </label>
-        <br />
-        <textarea
-          id='first'
-          className='input'
-          onInput={(e) => {
-            setFirstName(e.currentTarget.value);
-          }}
-          value={firstName}
-        ></textarea>
-        <br />
-        <label className='label' htmlFor='last'>
-          LAST NAME
-        </label>
-        <br />
-        <textarea
-          id='last'
-          className='input'
-          onInput={(e) => {
-            setLastName(e.currentTarget.value);
-          }}
-          value={lastName}
-        ></textarea>
-        <br />
-        <label className='label' htmlFor='location'>
-          LOCATION
-        </label>
-        <br />
-        <textarea
-          id='location'
-          className='input'
-          onInput={(e) => {
-            setLocation(e.currentTarget.value);
-          }}
-          value={location}
-        ></textarea>
-        <br />
-        <label className='label' htmlFor='item'>
-          ITEM LOST
-        </label>
-        <br />
-        <textarea
-          id='item'
-          className='input'
-          onInput={(e) => {
-            setItemName(e.currentTarget.value);
-          }}
-          value={itemName}
-        ></textarea>
-        <br />
-        <label className='label' htmlFor='details'>
-          ADDITIONAL INFORMATION
-        </label>
-        <br />
-        <textarea
-          id='details'
-          onInput={(e) => {
-            setDetails(e.currentTarget.value);
-          }}
-          value={details}
-        ></textarea>
-        <br />
-        <button id='button' onClick={submitForm}>
-          POST
-        </button>
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default CreatePost;
