ff5135b623f2e6e7e6113798ba4378126f182ac6
Arthur Huynh <arthurhuynh@Arthurs-Computer.local>
2023-02-01 11:40:12 -0500

    adds more styling

--- client/src/NavBar.jsx
+++ client/src/NavBar.jsx
@@ -3,12 +3,13 @@ import '../public/styles/navBar.scss';
 import raccoon from '../public/images/raccoon.png';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useHistory } from 'react-router-dom';
-
+import { RiLogoutBoxLine } from 'react-icons/ri';
 
 export default function NavBar() {
-  const { user, isAuthenticated, logout, loginWithRedirect, isLoading } = useAuth0();
+  const { user, isAuthenticated, logout, loginWithRedirect, isLoading } =
+    useAuth0();
   const [userPhoto, setUserPhoto] = useState();
-  const history = useHistory(); 
+  const history = useHistory();
 
   useEffect(() => {
     if (user) {
@@ -25,14 +26,17 @@ export default function NavBar() {
       </div>
       <div className='spacer'></div>
       <div className='right'>
-        <button id ='logout'
+        <button
+          id='logout'
           onClick={() =>
             logout({ logoutParams: { returnTo: window.location.origin } })
           }
         >
-          LOG OUT
+          <RiLogoutBoxLine size={35} />
         </button>
-        <button id='profileBtn' onClick={() => history.push('/profile')}><img id='userPhoto' src={userPhoto} /> </button>
+        <button id='profileBtn' onClick={() => history.push('/profile')}>
+          <img id='userPhoto' src={userPhoto} />{' '}
+        </button>
       </div>
     </div>
   );
